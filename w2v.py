3da1ad45f2463f1b92da997612bd1f23a8e94820
iarroyof <axayakac@gmail.com>
2016-02-29 17:07:51 -0600

    Added comprobation of the learned model.

--- w2v.py
+++ w2v.py
@@ -1,7 +1,7 @@
 # Author: Ignacio Arroyo-Fernandez (UNAM)
 
 from gensim.models import Word2Vec, Doc2Vec
-from gensim.models.doc2vec import TaggedLineDocument
+from gensim.models.doc2vec import TaggedLineDocument, LabeledSentence
 import os
 from argparse import ArgumentParser as ap
 import sys
@@ -21,12 +21,12 @@ def clean_Ustring_fromU(string):
     try: # Trying different cases for bad input documents.
         normalized_string = normalize('NFKC', gClean.lower())
     except TypeError:
-        sys.stderr.write('Bad formed string at the first attempt\n')
+        sys.stderr.write('Badly formed string at the first attempt\n-- Document sample: '+gClean[0:49])
         try:
             range_error = 999
             normalized_string = normalize('NFKC', gClean[0:range_error].lower()) # One thousand of characters are written if available. 
         except TypeError:
-            sys.stderr.write('\nThe wrong string at the second attempt: before %s words' % range_error)
+            sys.stderr.write('\nThe wrong string at the second attempt: before %s words\n' % range_error)
             try:
                 range_error = 99
                 normalized_string = normalize('NFKC', gClean[0:range_error].lower())
@@ -36,20 +36,26 @@ def clean_Ustring_fromU(string):
                     range_error = 49
                     normalized_string = normalize('NFKC', gClean[0:range_error].lower())
                 except TypeError:    
-                    sys.stderr.write('\nIt was not possible forming output file after three attempts. Fatally bad file')
+                    sys.stderr.write('\nIt was not possible forming output file after three attempts. Fatally bad file\n')
                     normalized_string = '# Fatally bad File\n'
                     pass
-    return  normalized_string # Return the unicode normalized document.
+    return  normalized_string.split() # Return the unicode normalized document.
 
 class yield_line_documents(object):
-    def __init__(self, dirname):
+    def __init__(self, dirname, d2v=False):
         self.dirname = dirname
-    
+        self.d2v = d2v
     def __iter__(self):
-        
-        for fname in os.listdir(self.dirname):
-            for line in open(os.path.join(self.dirname, fname)):
-                yield clean_Ustring_fromU(line).split()
+        if self.d2v:
+            for fname in os.listdir(self.dirname):
+                l = 1
+                for line in open(os.path.join(self.dirname, fname)):
+                    yield LabeledSentence(clean_Ustring_fromU(line), str(l)+"_"+fname)
+                    l += 1
+        else:
+            for fname in os.listdir(self.dirname):
+                for line in open(os.path.join(self.dirname, fname)):
+                    yield clean_Ustring_fromU(line)
 
 if __name__ == "__main__":
     parser = ap(description='Trains and saves a word2vec model into a file for mmap\'ing. Tokenization is performed un utf-8 an for Python 2.7. Non-latin characters are replaced by spaces. The model is saved into a given directory. All options are needed.')    
@@ -62,11 +68,19 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     if args.d2v:
-        articles = TaggedLineDocument(args.indir_file_name)
+        #articles = TaggedLineDocument(args.indir_file_name)
+        arts = yield_line_documents(args.indir_file_name, d2v = True)
+        articles = []
+        for a in arts:
+            articles.append(a)
         d2v_model = Doc2Vec(articles, min_count = args.minc, workers = args.threads, size = args.hidden)    
         d2v_model.save(args.outfile, separately = None)
     else:
         articles = yield_line_documents(args.indir_file_name)
         w2v_model = Word2Vec(articles, min_count = args.minc, workers = args.threads, size = args.hidden)
         w2v_model.save(args.outfile, separately = None)
-        	
+    
+    model = Doc2Vec.load(args.outfile)
+    del(model)
+    sys.stderr.write("\n>> Finished !!\n")
+        
