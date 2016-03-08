54b2be5f641684536766d7b63cf244d0d12f5574
iarroyof <axayakac@gmail.com>
2016-03-08 17:13:46 +0100

    SVR Results for headlines d2v sub, conc and conv. The conc operation seems to be more flexible. For test the generalization of models with FNWN, do generate functions for loading a SVR pretrained models.

--- svr.py
+++ svr.py
@@ -1,21 +1,29 @@
 import numpy as np
 from sklearn.svm import SVR
 from sklearn.grid_search import RandomizedSearchCV as RS
-from scipy.stats import sp_randint
+from scipy.stats import randint as sp_randint
+from scipy.stats import expon
 
 #inputfile = "/home/ignacio/data/vectors/pairs_headlines_d2v_H300_sub_m5.mtx"
 #inputfile = "/home/iarroyof/data/pairs_headlines_d2v_H300_sub_m5.mtx"
-inputfile = "/home/iarroyof/data/pairs_headlines_d2v_H300_convss_m5.mtx"
-gsfile = "/home/iarroyof/data/STS.gs.headlines.txt"
+from argparse import ArgumentParser as ap
+parser = ap(description='This script trains a SVR over any input dataset of numerical representations. The main aim is to determine a set of learning parameters')
+parser.add_argument("-x", help="Input file name (vectors)", metavar="input_file", required=True)
+parser.add_argument("-y", help="Regression  labels file.", metavar="regrLabs_file", required=True)
+parser.add_argument("-n", help="Number of tests to be performed.", metavar="tests_amount", default=1)
+parser.add_argument("-o", help="The operation the input data was derived from. Options: {'conc', 'convs', 'sub'}.", default="conc")
+args = parser.parse_args()
+#inputfile = "/home/iarroyof/data/pairs_headlines_d2v_H300_convss_m5.mtx"
+#gsfile = "/home/iarroyof/data/STS.gs.headlines.txt"
 #outputfile = "/home/iarroyof/sem_outputs/svr_output_headlines_100_d2v_conc_300_m5.txt"
-N = 5
-X = np.loadtxt(inputfile)
-y = np.loadtxt(gsfile)
-op = 'convs'
+N = int(args.n)
+X = np.loadtxt(args.x)
+y = np.loadtxt(args.y)
+op = args.o
 
 gammas = {
         'conc': expon(scale=10, loc=8.38049430369), 
-        'sub': expon(scale = 10, loc=15.1454004504), 
+        'sub': expon(scale = 20, loc=15.1454004504), 
         'convs':expon(scale = 50, loc = 541.113519625)}
 
 param_grid = [   
@@ -27,11 +35,12 @@ for n in xrange(N):
         svr = SVR()
         rs = RS(svr, param_distributions = params, n_iter = 10, n_jobs = 4)
         rs.fit(X, y)
-        y_est = rs.predict(X).tolist()
+        f_x = rs.predict(X).tolist()
 
         y_out = {}
-        y_out['estimated_output'] = y_est
+        y_out['estimated_output'] = f_x
         y_out['best_params'] = rs.best_params_
+        y_out['learned_model'] = {'support_vectors': rs.best_estimator_.support_.tolist(), 'alphas': rs.best_estimator_.dual_coef_[0].tolist()}
         y_out['best_score'] = rs.best_score_
 
         with open("svr_output_headlines_100_d2v_%s_300_m5.txt" % (op), "a") as f:
