b2730d1e6b430b65f5cb7a53fcbddd7a9a8f93f9
iarroyof <axayakac@gmail.com>
2016-03-16 15:29:20 +0100

    The svr is ready for tests on pretrained models. It is pending to predict from vector files inside a whole directory.

--- svr.py
+++ svr.py
@@ -1,4 +1,4 @@
-import numpy as np
+#v$import numpy as np
 from sklearn.svm import SVR
 from sklearn.grid_search import RandomizedSearchCV as RS
 from scipy.stats import randint as sp_randint
@@ -15,9 +15,9 @@ min_count = "m10"
 from argparse import ArgumentParser as ap
 parser = ap(description='This script trains a SVR over any input dataset of numerical representations. The main aim is to determine a set of learning parameters')
 parser.add_argument("-x", help="Input file name (vectors)", metavar="input_file", required=True)
-parser.add_argument("-y", help="Regression  labels file.", metavar="regrLabs_file", required=True)
+parser.add_argument("-y", help="Regression labels file. Do not specify this argument if you want to uniauely predict over any test set. In this case, you must to specify the SVR model to be loaded as the parameter of the option -o.", metavar="regrLabs_file")
 parser.add_argument("-n", help="Number of tests to be performed.", metavar="tests_amount", default=1)
-parser.add_argument("-o", help="The operation the input data was derived from. Options: {'conc', 'convs', 'sub'}. In the case you want to give a precalculated center for width randomization, specify the number. e.g. '-o 123.654'.", default="conc")
+parser.add_argument("-o", help="The operation the input data was derived from. Options: {'conc', 'convs', 'sub'}. In the case you want to give a precalculated center for width randomization, specify the number. e.g. '-o 123.654'. A filename can be specified, which is the file where a SVR model is sotred, e.g. '-o filename.model'", default="conc")
 args = parser.parse_args()
 #inputfile = "/home/iarroyof/data/pairs_headlines_d2v_H300_convss_m5.mtx"
 #gsfile = "/home/iarroyof/data/STS.gs.headlines.txt"
@@ -29,10 +29,26 @@ op = args.o
 
 if op.replace('.','',1).isdigit():
     op = 'esp'
+elif not op in gammas:
+    from os.path import basename, splitext
+    infile = basename(op)
+    if infile and infile != "*":
+        
+        filename = splitext(infile)[0]+'_predict.out'
+        model = joblib.load(op, 'r')
+        y_out = {}
+        y_out['estimated_output'] = model.predict(X).tolist()
+        # Add more metadata to the dictionary as reauired.
+        with open(filename, 'a') as f:
+            f.write(str(y_out)+'\n')
+        exit()
+    else:
+        print "Please specify a file name for loading the SVR pretrained model."            
+        exit()
 
 gammas = {
-        'conc': expon(scale=10, loc=8.38049430369), 
-        'sub': expon(scale = 20, loc=15.1454004504), 
+        'conc': expon(scale=10, loc=8.38049430369),
+        'sub': expon(scale = 20, loc=15.1454004504),
         'convs':expon(scale = 50, loc = 541.113519625),
         'esp':expon(scale = 20, loc = float(args.o)) }
 
