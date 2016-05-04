03943164c0487b7d965ee180f41535d2c2547de6
iarroyof <axayakac@gmail.com>
2016-05-04 06:35:26 -0500

    Added the estimations for fussion puces miguel elephants.

--- dem_splitter.py
+++ dem_splitter.py
@@ -1,4 +1,11 @@
+from argparse import ArgumentParser as ap
 
-with open("/almac/ignacio/data/puces_dem.txt", 'r') as fi, open("/almac/ignacio/data/puces_scores_per.txt", 'w') as fo:   
+parser = ap(description='This script performs different tests over any input dataset of numerical representations. The main aim is to determine an estimate of a valid')
+parser.add_argument("-f", help="Input file name (complete democratic)", metavar="input_file", required=True)
+parser.add_argument("-F", help="Output file name (scores complete)", metavar="output_file", required=True)
+
+args = parser.parse_args()
+
+with open(args.f, 'r') as fi, open(args.F, 'w') as fo:   
     for line in fi:
         fo.write("%s" % (line.split("\t")[1]))
