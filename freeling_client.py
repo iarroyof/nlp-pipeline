ad6cfe0961036602e014efda9869f2442cd1f923
iarroyof <axayakac@gmail.com>
2016-05-09 08:29:04 -0500

    added the modified freeLing client

--- freeling_client.py
+++ freeling_client.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import requests
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", help="Input file name (document to be PoS tagged.)", metavar="input_file", required=True)
+
+args = parser.parse_args()
+
 #Archivo a ser enviado
-files = {'file': open('sushi_fr.txt', 'rb')}
+files = {'file': open(args.f, 'rb')}
 #Parámetros posibles
 #outf: tagged, tagged_en, tagged_fr
 #format: json, plain, html
