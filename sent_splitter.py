42eff09c974cc623006f178ccdc6f619f409b836
iarroyof <axayakac@gmail.com>
2016-06-16 14:51:29 +0200

    sentence splitter adecquated for DUC corpora.*

--- sent_splitter.py
+++ sent_splitter.py
@@ -1,4 +1,5 @@
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+from nltk.data import load
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -7,12 +8,13 @@ parser.add_argument("-o", help="Output file name (sentence by row)", metavar="ou
 
 args = parser.parse_args()
 
-punkt_param = PunktParameters()
-punkt_param.abbrev_types = set(['jr', 'dr', 'vs', 'mr', 'mrs', 'prof', 'inc','e.g', 'i.e', 'gen', 'al', 'ph', 'd', 'c', 'd', 'h', 'k', 'm', 'mme', 'ms', 'msc', 'vol', 'cit', 'pp', 'ed', 'cap', 'p', 'r', 's', 'w', 'cf', 'l', 'n', 'e', 'g', 'i', 'v', 'chap', 'ch', 'i', 'no', 'st', 't', 'j', 'f', '1o', '2o', 'phd'])
-sent_tokenize = PunktSentenceTokenizer(punkt_param)
+#punkt_param = PunktParameters()
+#punkt_param.abbrev_types = set(['jr', 'dr', 'vs', 'mr', 'mrs', 'prof', 'inc','e.g', 'i.e', 'gen', 'al', 'ph', 'd', 'c', 'd', 'h', 'k', 'm', 'mme', 'ms', 'msc', 'vol', 'cit', 'pp', 'ed', 'cap', 'p', 'r', 's', 'w', 'cf', 'l', 'n', 'e', 'g', 'i', 'v', 'chap', 'ch', 'i', 'no', 'st', 't', 'j', 'f', '1o', '2o', 'phd'])
+#sent_tokenize = PunktSentenceTokenizer(punkt_param)
+sent_tokenize = load("tokenizers/punkt/english.pickle")
 
 with open(args.i) as f:
-    lines = map(lambda str : str.replace('. »', '.»').replace('. "', '."').replace('? "', '?"').replace('? »', '?»').replace('! "', '!"').replace('! »', '!»'), f.readlines())
+    lines = map(lambda str : str.replace('\t', ' ').replace('\n', ' ').replace('. »', '.»').replace('. "', '."').replace('? "', '?"').replace('? »', '?»').replace('! "', '!"').replace('! »', '!»'), f.readlines())
 
 with open(args.o,"w") as f:
     for line in lines:
