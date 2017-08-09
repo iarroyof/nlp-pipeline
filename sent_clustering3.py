ed0b3044466118d13e99285aaa287b740e36ba15
Ignacio Arroyo <axayakac@gmail.com>
2017-08-09 09:42:48 -0500

    Last version clustering kmeans with cosine distance

--- sent_clustering3.py
+++ sent_clustering3.py
@@ -8,24 +8,29 @@ import logging
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-
 import sys
 import numpy as np
 from argparse import ArgumentParser as ap
 import os
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
+
 parser = ap(description='This script trains/applies a SVR over any input dataset of numerical representations. The main aim is to determine a set of learning parameters')
 parser.add_argument("-x", help="Input file name (vectors)", metavar="input_file", required=True)
+parser.add_argument("-m", help="Clustering metric [cosine|euclidean]", metavar="metric", default="euclidean")
 parser.add_argument("-c", help="Clusterer = {km, km++, aggwr}", metavar="clusterer", required=True)
 parser.add_argument("-k", help="Number of clusters", metavar="k_clusters")
 parser.add_argument("-N", help="Number of trials for maximize Silhueltte", metavar="n_trials")
 parser.add_argument("-t", default=False, action="store_true", help="Toggle if labels are PoS tags instead of snippets.")
 parser.add_argument("-n", default=False, action="store_true", help="Toggle if labels are NounPhrases instead of snippets.")
-#parser.add_argument("-y", help="""Regression labels file. Do not specify this argument if you want to uniauely predict over any test set. In this case, you must to specify
-#                                the SVR model to be loaded as the parameter of the option -o.""", metavar="regrLabs_file", default = None)
 args = parser.parse_args()
 
 min_show_length = 100
+
+if args.m=="cosine":
+    from sklearn.metrics.pairwise import cosine_distances as cos
+    KMeans.euclidean_distances=cos
 
 def cleaner(line): # The default is the average sentence length in English
     return line.strip()#[:min_show_length]
@@ -78,28 +83,23 @@ if args.c.startswith("km"):
     num_clusters = int(args.k)
 
     if "++" in args.c:
-        km = KMeans(n_clusters=num_clusters, init='k-means++', max_iter=100, n_init=1)
+        km = KMeans(n_clusters=num_clusters, init='k-means++', max_iter=100,
+                                                            verbose=1, n_init=1)
         km.fit(X)
         coeff = metrics.silhouette_score(X, km.labels_, sample_size=1000)
         clusters=km.labels_.tolist()
     else:
         max_silhouette = -1
-
-
         silhouette = -1
-#        N_clusters = 2
         for tr in range(int(args.N)): # Number of trials for maximize Silhueltte
-            
-            #for N_cl in range(3,num_clusters):
-             km = KMeans(n_clusters=num_clusters, init='random', max_iter=100, n_init=1)
+             km = KMeans(n_clusters=num_clusters, init='random', max_iter=100,
+                                                verbose=1, n_init=1, n_jobs=4)
              km.fit(X)
              coeff = metrics.silhouette_score(X, km.labels_, sample_size=1000)
-             print ("Partial Silhuette: %f" % coeff)
-             if silhouette < coeff: 
+             #print ("Partial Silhuette: %f" % coeff)
+             if silhouette < coeff:
                  clusters=km.labels_.tolist()
                  silhouette = coeff
-             #    N_clusters = N_cl
-    #order_centroids = km.cluster_centers_.argsort()[:, ::-1]
     definitions=sorted(list(zip(clusters, snippets)), key=lambda x: x[0])
 
     while(1):
@@ -108,22 +108,14 @@ if args.c.startswith("km"):
             print ("%d\t%s" % (c, s))
         except IndexError:
             break
-    
-#    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
-#    print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
-#    print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
-#    print("Adjusted Rand-Index: %.3f" % metrics.adjusted_rand_score(labels, km.labels_))
+
     print("Silhouette Coefficient: %0.3f" % silhouette)
     print("Number of clusters: %d" % num_clusters)
     print()
 
- 
 elif args.c.startswith("agg"):
-    #if "wr" in args.c:
     dist = 1 - cosine_similarity(X)
-
     linkage_matrix = ward(dist) #define the linkage_matrix using ward clustering pre-computed distances
-
     fig, ax = plt.subplots(figsize=(15, 20)) # set size
     ax = dendrogram(linkage_matrix, orientation="right", labels=list(snippets));
 
@@ -137,4 +129,3 @@ elif args.c.startswith("agg"):
     plt.tight_layout() #show plot with tight layout
 #uncomment below to save figure
     plt.savefig("ward_clusters_%s%s_%s_H%s.png" % (term_name, t, corpus, dimensions), dpi=200) #save figure as ward_clusters
-    #plt.close()
